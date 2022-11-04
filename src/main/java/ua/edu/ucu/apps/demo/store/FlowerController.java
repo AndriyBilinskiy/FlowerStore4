0821b92e0590234476fb3911c5c8d2a81d3a95c3
andriy <andriy@intteh.com>
2022-11-04 21:31:39 +0200

    final

--- src/main/java/ua/edu/ucu/apps/demo/store/FlowerController.java
+++ src/main/java/ua/edu/ucu/apps/demo/store/FlowerController.java
@@ -3,10 +3,16 @@ package ua.edu.ucu.apps.demo.store;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ua.edu.ucu.apps.demo.payments.PayPalPayment;
+import ua.edu.ucu.apps.demo.payments.Payment;
 
 import java.util.List;
 
 @RequestMapping("/payment")
 @RestController
 public class FlowerController {
+    @GetMapping
+    public List<FlowerBucket> getBuckets(){
+        return List.of(new FlowerBucket());
+    }
 }
