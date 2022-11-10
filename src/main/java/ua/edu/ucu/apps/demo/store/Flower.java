a925f2fb4d275c07c6e341e4f12053fdd867ae03
andriy <andriy@intteh.com>
2022-11-10 10:39:53 +0200

    com

--- src/main/java/ua/edu/ucu/apps/demo/store/Flower.java
+++ src/main/java/ua/edu/ucu/apps/demo/store/Flower.java
@@ -1,12 +1,24 @@
 package ua.edu.ucu.apps.demo.store;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.apache.logging.log4j.message.AsynchronouslyFormattable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 
 @Setter
+@Entity
+@Table
+@NoArgsConstructor
 public class Flower {
+    @Id
+    @GeneratedValue
+    private int id;
     @Getter
     private double sepalLength;
     private FlowerColor color;
