2b50b23650b15ffc35e473f71efee92324a056d4
andriy <andriy@intteh.com>
2022-11-11 22:22:02 +0200

    initial

---
+++ src/main/java/ua/edu/ucu/apps/demo/flower/FlowerService.java
@@ -0,0 +1,14 @@
+package ua.edu.ucu.apps.demo.flower;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class FlowerService {
+    @Autowired
+    private FlowerRepository flowerRepository;
+    public List<Flower> getFlowers(){
+        return flowerRepository.findAll();
+    }
+}
